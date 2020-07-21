0992c70686bad5cf44a62df74ec4d64390a8dfc5
sai <sai@mohan>
2020-07-21 07:52:10 +0530

    ims Eureka

---
+++ src/test/java/com/cts/servicediscovery/ImsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
